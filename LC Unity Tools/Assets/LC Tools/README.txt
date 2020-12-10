3886e91748a90a8855ae7e1d6eb512e23ab5d967
LoucaColes_ <coleslouca@gmail.com>
2020-12-10 20:29:58 +0000

    [LOUCA] [ADD] Added scene switcher tool.

---
+++ LC Unity Tools/Assets/LC Tools/README.txt
@@ -0,0 +1,15 @@
+|-------------------------|
+|LC Tools - Scene Switcher|
+|-------------------------|
+
+How To Use:
+* Go to Tools->LC Tools->SceneSwitcher
+* Click Add scene, this will create a new scene entry
+* Assign a scene that you will be working on
+* Repeat process until all scene entries needed have been created
+* To remove a scene entry click remove scene from list
+* Any scene added can then be opened/opened additively
+
+Notes:
+* In the settings there is a toggle for auto save, this should save any scenes that are closed via the Scene Switcher Window. This should work without any issues but it is worth saving just in case.
+* In the settings, there is also a toggle for how scenes are open additively, if on, the scene will be loaded otherwise the scene will be added but unloaded.
---
+++ LC Unity Tools/Assets/LC Tools/Scene Switcher/Editor/SceneSwitcherWindow.cs
@@ -0,0 +1,115 @@
+﻿// Liscense - GPL-3.0-or-later
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+public class SceneSwitcherWindow : EditorWindow
+{
+    private bool saveOnClose = false;
+    private bool openAdditiveLoaded = true;
+    private Vector2 scrollPos;
+    private List<SceneAsset> scenes = new List<SceneAsset>();
+    private List<SceneAsset> scenesToRemove = new List<SceneAsset>();
+
+    [MenuItem("Tools/LC Tools/Scene Switcher")]
+    static void Init()
+    {
+        // Get existing open window or if none, make a new one:
+        SceneSwitcherWindow window = (SceneSwitcherWindow)EditorWindow.GetWindow(typeof(SceneSwitcherWindow));
+        window.Show();
+    }
+
+    void OnGUI()
+    {
+        DisplaySettings();
+
+        EditorGUILayout.Space();
+
+        EditorGUILayout.LabelField("Scenes");
+
+        EditorGUILayout.BeginHorizontal();
+        scrollPos = EditorGUILayout.BeginScrollView(scrollPos, GUILayout.Width(position.width - 10), GUILayout.Height(position.height - 10));
+
+        DisplaySceneEntries();
+
+        EditorGUILayout.Space();
+
+        if (GUILayout.Button("Add Scene"))
+        {
+            scenes.Add(null);
+        }
+
+        if (GUILayout.Button("Clear All Scenes"))
+        {
+            scenes.Clear();
+        }
+
+        EditorGUILayout.EndScrollView();
+        EditorGUILayout.EndHorizontal();
+    }
+
+    private void DisplaySceneEntries()
+    {
+        scenesToRemove.Clear();
+
+        for (int index = 0; index < scenes.Count; index++)
+        {
+            scenes[index] = (SceneAsset)EditorGUILayout.ObjectField("Scene: ", scenes[index], typeof(SceneAsset), true);
+
+            if (GUILayout.Button("Open Scene"))
+            {
+                string scenePath = AssetDatabase.GetAssetPath(scenes[index]);
+                if (!saveOnClose && EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+                {
+                    EditorSceneManager.OpenScene(scenePath);
+                }
+                else if (saveOnClose)
+                {
+                    EditorSceneManager.MarkAllScenesDirty();
+                    EditorSceneManager.SaveOpenScenes();
+                    EditorSceneManager.OpenScene(scenePath);
+                }
+            }
+
+            if (GUILayout.Button("Open Additively"))
+            {
+                string scenePath = AssetDatabase.GetAssetPath(scenes[index]);
+                OpenSceneMode mode = openAdditiveLoaded ? OpenSceneMode.Additive : OpenSceneMode.AdditiveWithoutLoading;
+                EditorSceneManager.OpenScene(scenePath, mode);
+            }
+
+            if (GUILayout.Button("Remove Scene From List"))
+            {
+                scenesToRemove.Add(scenes[index]);
+            }
+
+            EditorGUILayout.Space();
+        }
+
+        for (int index = 0; index < scenesToRemove.Count; index++)
+        {
+            scenes.Remove(scenesToRemove[index]);
+        }
+    }
+
+    private void DisplaySettings()
+    {
+        EditorGUILayout.LabelField("Settings");
+
+        if (saveOnClose)
+        {
+            EditorGUILayout.HelpBox("Warning: while autosave will most likely work when switching scenes," +
+                " it is suggested to save manually as well", MessageType.Warning);
+        }
+        saveOnClose = EditorGUILayout.Toggle("Auto Save: ", saveOnClose);
+
+        if (!openAdditiveLoaded)
+        {
+            EditorGUILayout.HelpBox("Warning: If you open a scene additively, it will open unloaded", MessageType.Warning);
+        }
+        openAdditiveLoaded = EditorGUILayout.Toggle("Additive Scenes Loaded: ", openAdditiveLoaded);
+    }
+}
