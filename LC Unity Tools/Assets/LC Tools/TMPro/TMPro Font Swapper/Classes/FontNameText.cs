ceac02fc7b6d900cfe1d1087c97747202cfdf3ff
LoucaColes_ <coleslouca@gmail.com>
2020-12-30 12:18:44 +0000

    [LOUCA] [ADD] Added some test fonts and created font assets from them [ADD] Implemented font swapper logic

---
+++ LC Unity Tools/Assets/LC Tools/TMPro/TMPro Font Swapper/Classes/FontNameText.cs
@@ -0,0 +1,52 @@
+﻿// Liscense - GPL-3.0-or-later
+
+using TMPro;
+using UnityEngine;
+
+namespace LC.Tools.TMProFontSwapper
+{
+    public class FontNameText : MonoBehaviour
+    {
+        private TextMeshProUGUI textUGUI = null;
+        private int fontId = 0;
+
+        private void Awake()
+        {
+            textUGUI = GetComponent<TextMeshProUGUI>();
+        }
+
+        // Start is called before the first frame update
+        private void Start()
+        {
+            TMProFontSwapperManager.OnFontChangedDelegate += UpdateFontAsset;
+        }
+
+        private void OnDestroy()
+        {
+            TMProFontSwapperManager.OnFontChangedDelegate -= UpdateFontAsset;
+        }
+
+        private void OnEnable()
+        {
+            if (TMProFontSwapperManager.instance == null) { return; }
+
+            // Check if the font has changed since last shown
+            if (fontId == TMProFontSwapperManager.instance.CurrentFontId) { return; }
+
+            // If changed then update with new font
+            textUGUI.font = TMProFontSwapperManager.instance.GetCurrentFontAsset();
+            fontId = TMProFontSwapperManager.instance.CurrentFontId;
+        }
+
+        /// <summary>
+        /// Update the text component's text value with font asset's name
+        /// </summary>
+        /// <param name="_newAsset">New font asset for text component</param>
+        public void UpdateFontAsset(TMP_FontAsset _newAsset, int _id)
+        {
+            textUGUI.text = _newAsset.name;
+            fontId = _id;
+        }
+    }
+
+}
---
+++ LC Unity Tools/Assets/LC Tools/TMPro/TMPro Font Swapper/Classes/FontSwapperData.cs
@@ -0,0 +1,13 @@
+﻿// Liscense - GPL-3.0-or-later
+
+using TMPro;
+using UnityEngine;
+
+namespace LC.Tools.TMProFontSwapper
+{
+    [CreateAssetMenu(fileName = "Font Swapper Data", menuName = "LC Tools/Scriptable Objects/Font Swapper Data")]
+    public class FontSwapperData : ScriptableObject
+    {
+        public TMP_FontAsset[] fontAssets = new TMP_FontAsset[0];
+    } 
+}
